f55e75574903562f30b86d7d5bdc359191b49a3e
Guilherme Campos <gcordeiro@id.uff.br>
2018-07-08 16:31:07 -0300

    Configuração controller

--- src/main/java/processo/fattoria/desafio/service/ProdutoService.java
+++ src/main/java/processo/fattoria/desafio/service/ProdutoService.java
@@ -5,9 +5,8 @@
  */
 package processo.fattoria.desafio.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import processo.fattoria.desafio.DAO.ProdutoDAO;
 import processo.fattoria.desafio.modelo.Produto;
@@ -26,8 +25,8 @@ public class ProdutoService {
         return produtoDAO.buscarPorNome(nome);
     }
 
-    public Page<Produto> buscarTodos(Pageable pgbl) {
-        return produtoDAO.findAll(pgbl);
+    public List<Produto> buscarTodos() {
+        return produtoDAO.findAll();
     }
 
     public Produto buscarPorId(Integer id) {

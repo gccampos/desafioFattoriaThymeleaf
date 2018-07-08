741c017e1931d6dc1a3b9691fec26b626afdb548
Guilherme Campos <gcordeiro@id.uff.br>
2018-07-08 17:50:01 -0300

    Commit final, de fato

--- src/main/java/processo/fattoria/desafio/controller/ProdutoController.java
+++ src/main/java/processo/fattoria/desafio/controller/ProdutoController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import processo.fattoria.desafio.modelo.Produto;
 import processo.fattoria.desafio.service.ProdutoService;
 
@@ -30,7 +31,6 @@ import processo.fattoria.desafio.service.ProdutoService;
  * @author usuario
  */
 @Controller
-@RequestMapping("/produto")
 public class ProdutoController {
 
     @Autowired
@@ -84,8 +84,8 @@ public class ProdutoController {
         return "redirect:thymeleaf";
     }
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET, produces =  "text/Html")
     public String getHome() {
-        return "home";
+        return "/home";
     }
 }
